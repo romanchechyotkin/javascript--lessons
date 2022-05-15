2e2265348b3cde1718010ad74a9469c02ebea430
Roman Chechytokin <romanchechyotkin@gmail.com>
2022-05-15 15:18:48 +0300

    2nd lesson

--- 02_programmeStructure/app.js
+++ 02_programmeStructure/app.js
@@ -62,3 +62,33 @@ for(let number = 20; ; number = number + 1){
     }
 }
 
+console.log('Построение треугольника в цикле');
+let hash = '#'
+for(let counter = 1; counter <= 7; counter++){
+    console.log(hash);
+    hash = hash + '#'
+}
+
+console.log('FizzBuzz'); 
+/* 
+    Напишите программу, в которой с помощью console.log выводятся все чис-
+    ла от 1 до 100 с двумя исключениями. Для чисел, кратных 3, вместо числа
+    выводится "Fizz", а для чисел, кратных 5 (но не 3), - "Buzz".
+*/
+
+number = 1
+while(number <= 100){
+    if(number % 5 == 0 && number % 3 == 0){
+        console.log(number, 'FizzBuzz');
+    }
+    else if(number % 3 == 0){
+        console.log(number, 'Fizz');
+    }
+    else if(number % 5 == 0){
+        console.log(number, 'Buzz');
+    }
+    else{
+        console.log(number);
+    }
+    number++
+}

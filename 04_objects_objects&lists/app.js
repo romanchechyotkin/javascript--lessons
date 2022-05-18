d2537e3608c5b5c28e417ae0ff2468cfbb991ef0
Roman Chechytokin <romanchechyotkin@gmail.com>
2022-05-18 22:15:51 +0300

    4th lesson

---
+++ 04_objects_objects&lists/app.js
@@ -0,0 +1,9 @@
+// массив 
+let numberList = [1, 2, 3, 5, 66, 100]
+console.log(numberList[0], numberList[4], numberList[5]); // 1 66 100
+console.log(numberList.length); // длина массива
+numberList.push(0) // добавляет элемент в массив 
+numberList.push(11)
+console.log(numberList);
+console.log(numberList.pop()); // удаляет последний в массиве элемент
+console.log(numberList);

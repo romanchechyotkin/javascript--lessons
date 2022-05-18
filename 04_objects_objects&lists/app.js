b12dcd17a6bc755ac48475b1c621c32f5e8b3002
Roman Chechytokin <romanchechyotkin@gmail.com>
2022-05-18 22:28:36 +0300

    4th lesson

--- 04_objects_objects&lists/app.js
+++ 04_objects_objects&lists/app.js
@@ -7,3 +7,19 @@ numberList.push(11)
 console.log(numberList);
 console.log(numberList.pop()); // удаляет последний в массиве элемент
 console.log(numberList);
+
+// объект
+let day1 = {
+    squirrel: false,
+    events: ['работал', 'бегал', 'ел', 'трогал дерево']
+}
+
+console.log(day1.events);
+console.log(day1.squirrel);
+console.log(day1.wolf);
+day1.wolf = true // записав имя объекта и через точку имя нового свойства, создается заданное свойство 
+console.log(day1.wolf);
+delete day1.squirrel // удаляет свойство из объекта
+console.log(day1.squirrel);
+console.log('squirrel' in day1); // проверка на наличие в объекте
+console.log('wolf' in day1);
